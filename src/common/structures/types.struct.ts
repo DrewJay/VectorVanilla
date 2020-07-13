e7e0db6a61618a2a814e3580772dfee880a62509
Andrej Noskaj <andrej.nosky@gmail.com>
2020-07-13 21:56:03 +0200

    More code prettyfying.

--- src/common/structures/types.struct.ts
+++ src/common/structures/types.struct.ts
@@ -1,57 +1,57 @@
 /**
  * Normalization funciton type.
  */
-export type NormalizationFunction = (input: number) => number;
+export type NormalizationFunction = (input: number) => number
 
 /**
  * Activation function structure.
  */
-export type ActivationFunction = (input: number, param?: number) => number;
+export type ActivationFunction = (input: number, param?: number) => number
 
 /**
  * Error function structure.
  */
-export type CostFunction = (target: number, output: number) => number;
+export type CostFunction = (target: number, output: number) => number
 
 /**
  * Derivative function structure.
  */
-export type DerivativeFunction = ActivationFunction;
+export type DerivativeFunction = ActivationFunction
 
 /**
  * Range-random generator function.
  */
-export type RRGenerator = (low: number, high: number, special?: boolean) => number;
+export type RRGenerator = (low: number, high: number, special?: boolean) => number
 
 /**
  * Activation functions string representations.
  */
-export type ActivationFunctionTypes = 'ReLu' | 'sigmoid' | 'linear';
+export type ActivationFunctionTypes = 'ReLu' | 'sigmoid' | 'linear'
 
 /**
  * Error function string representations.
  */
-export type CostFunctionTypes = 'meanSquaredError';
+export type CostFunctionTypes = 'meanSquaredError'
 
 /**
  * Collection of activation functions.
  */
 export type ActivationFunctionsCollection = {
-    [key in ActivationFunctionTypes]: ActivationFunction;
+    [key in ActivationFunctionTypes]: ActivationFunction
 }
 
 /**
  * Collection of activation functions.
  */
 export type CostFunctionsCollection = {
-    [key in CostFunctionTypes]: CostFunction;
+    [key in CostFunctionTypes]: CostFunction
 }
 
 /**
  * Collection of derivative functions.
  */
 export type DerivativeFunctionsCollection = {
-    [key in ActivationFunctionTypes]: DerivativeFunction;
+    [key in ActivationFunctionTypes]: DerivativeFunction
 }
 
 /**
@@ -61,7 +61,7 @@ export type Layer = {
     nodes: number;
     activation?: ActivationFunctionTypes;
     bias: number;
-};
+}
 
 /**
  * Layer node object.
@@ -72,12 +72,12 @@ export type Node = {
     weightedSum: number;
     connectedTo: Connection[];
     connectedBy: Connection[];
-};
+}
 
 /**
  * Particular flags for node collections.
  */
-export type NodeFlags = 'input' | 'output';
+export type NodeFlags = 'input' | 'output'
 
 /**
  * Node collection object wrapper.
@@ -88,7 +88,7 @@ export type NodeGroup = {
     bias: number;
     flags: NodeFlags[];
     error?: number;
-};
+}
 
 /**
  * Group of connected-to/by nodes.
@@ -96,4 +96,4 @@ export type NodeGroup = {
 export type Connection = {
     node: Node;
     weight: number | null;
-};
+}
